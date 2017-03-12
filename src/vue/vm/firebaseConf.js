74cbe918e049f2a390cc415cf4fdc2ed8a3dd0d9
Voytov Stanislav <gonlliuk@gmail.com>
2017-03-12 20:08:57 +0500

    changes

--- src/vue/vm/firebaseConf.js
+++ src/vue/vm/firebaseConf.js
@@ -1,7 +1,7 @@
 export default {
-    apiKey: "AIzaSyCssiUJC7M4fAV8qGsgtrvnHywS4Zc7E_k",
+    apiKey: process.env.GOOGLE_KEY || '',
     authDomain: "planner-faf0d.firebaseapp.com",
     databaseURL: "https://planner-faf0d.firebaseio.com",
     storageBucket: "planner-faf0d.appspot.com",
-    messagingSenderId: "98892695131"
+    messagingSenderId: process.env.GOOGLE_SENDER || ''
 };
